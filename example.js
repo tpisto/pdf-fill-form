38dffe440743c29ec5fdf386fd22272de7cd8783
Tommi Pisto <tommi@pis.to>
2015-04-06 20:28:16 +0300

    Ran through Google Closure linter

--- example.js
+++ example.js
@@ -2,15 +2,19 @@ var pdfFormFill = require('lib/pdf-fill-form.js');
 var fs = require('fs');
 
 // Show fields
-var formFields = pdfFormFill.readSync('test.pdf')
+var formFields = pdfFormFill.readSync('test.pdf');
 console.log(formFields);
 
 // Write fields
-pdfFormFill.writeAsync('test.pdf', { "myField1": "myField1 fill text" }, { "save": "imgpdf" }, function(err, result) {
-  fs.writeFile("test_filled_images.pdf", result, function(err) {
-    if(err) {
+pdfFormFill.writeAsync('test.pdf', { 'myField1': 'myField1 fill text' }, { 'save': 'imgpdf' }, function(err, result) {
+  if (err) {
+      return console.log(err);
+  }
+
+  fs.writeFile('test_filled_images.pdf', result, function(err) {
+    if (err) {
         return console.log(err);
     }
-    console.log("The file was saved!");
-  }); 
+    console.log('The file was saved!');
+  });
 });
